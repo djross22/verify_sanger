3b75027259c700a03a4ecbc60ce00f4aa2dccf99
David Ross <david.ross@nist.gov>
2021-07-25 17:00:27 -0400

    Add alternate constructor method to makle PlottableRecord from alignment

--- verify_sanger/PlottableRecord.py
+++ verify_sanger/PlottableRecord.py
@@ -8,6 +8,7 @@ Created on Sun Jul 18 19:42:27 2021
 import numbers
 import numpy as np
 from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 
 # sanger_channels correspond to the ABI data traces for the letters: G, A, T, C
 sanger_channels = ["DATA9", "DATA10", "DATA11", "DATA12"]
@@ -57,6 +58,8 @@ class PlottableRecord(SeqRecord):
             
             self._chrom_data = [chrom_x, chrom_g, chrom_a, 
                                 chrom_t, chrom_c]
+        else:
+            self._chrom_data = [np.array([np.nan]*len(self))]
             
     
     def __getitem__(self, index):
@@ -85,6 +88,77 @@ class PlottableRecord(SeqRecord):
         new_record._chrom_data = new_data
         
         return new_record
+    
+    @classmethod
+    def make_aligned_record(cls, input_record, aligned_seq):
+        """
+        The method is effectively an alternate constructor for making a new
+        PlottableRecord instance based on a sequence alignment
+
+        Parameters
+        ----------
+        input_record : sequence record, PlotttableRecord or Bio.SeqRecord.SeqRecord
+            The sequence record used as input to the alignment.
+        aligned_seq : DNA sequence, Bio.Seq.Seq or str
+            The aligned sequence, with possible gaps.
+
+        Raises
+        ------
+        ValueError
+            aligned_seq must result from an alignment of input_record. So, 
+            after ungapping, aligned_seq must be the same sequence as input_record.
+
+        Returns
+        -------
+        new_record : PlottableRecord
+            The new plottable sequence record, adjusted to match the alignment
+            (i.e. gaps insterted into the quality and chromatogram data, etc.).
+
+        """
+        
+        aligned_seq = str(aligned_seq)
+        input_seq = str(input_record.seq)
+        
+        # Check that align_str contains the same sequence as input_record, with possible gaps
+        if aligned_seq.replace('-', '') != input_seq.replace('-', ''):
+            raise ValueError("After ungapping, aligned_seq must be the same sequence as input_record.")
+        
+        new_record = SeqRecord(seq=Seq(aligned_seq),
+                               id=input_record.id,
+                               name=input_record.name,
+                               description=input_record.description,
+                               dbxrefs=input_record.dbxrefs,
+                               features=input_record.features,
+                               annotations=input_record.annotations,
+                               letter_annotations=None,
+                               )
+        
+        new_record = PlottableRecord(new_record)
+        
+        # Loop over all associated lists/arrays that have an entry for 
+        #     each base in the sequence
+        for k, v in input_record.letter_annotations.items():
+            new_record.letter_annotations[k] = align_data(v, input_seq, aligned_seq)
+        
+        #new_record.coverage = align_data(new_record.coverage, input_seq, aligned_seq, align_str)
+        
+        new_data = []
+        
+        data = input_record._chrom_data[0]
+        data = np.array( align_data(data, input_seq, aligned_seq, fill_item=np.array([0, 1])) )
+        x_shift = [x[0]-i-0.5 for i, x in enumerate(data)]
+        new_data.append(data - x_shift)
+        
+        for data in input_record._chrom_data[1:]:
+            data = np.array( align_data(data, input_seq, aligned_seq, fill_item=np.array([np.nan, np.nan])) )
+            new_data.append(data)
+            
+        new_record._chrom_data = new_data
+        
+        new_record.seq = Seq(aligned_seq)
+        
+        return new_record
+    
     
     def chromatogram_plot_data(self, start=0, end=None):
         if end == None:
@@ -156,3 +230,45 @@ def peak_edges(peak_locations):
     peak_right = np.array([int(np.round(x)) if x>0 else 0 for x in peak_right])
     
     return peak_left, peak_right
+
+
+def align_data(data, input_seq, aligned_seq, fill_item=0):
+    """
+    Method to create new lists of data (letter annotations, chromatogram data)
+        based on an alignment (i.e. insterting gaps, etc.)
+
+    Parameters
+    ----------
+    data : array like
+        the data to be aligned.
+    input_seq : string
+        The DNA sequence of the input record used for the alignment.
+    aligned_seq : string
+        The aligned sequence, with possible gaps.
+
+    Returns
+    -------
+    new_data : list
+        the resulting aligned data.
+
+    """
+    
+    if len(data) != len(input_seq):
+        raise ValueError(f"The length of data ({len(data)}) must equal the length of input_seq ({len(input_seq)})")
+        
+    new_data = []
+    for b1 in aligned_seq:
+        # gap
+        if b1 == '-':
+            new_data.append(fill_item)
+        else:
+            if len(data) == 0:
+                raise ValueError(f'Unexpected non-gap base found in aligned_seq ({b1}) after end of data')
+            elif b1 == input_seq[0]:
+                new_data.append(data[0])
+                data = data[1:]
+                input_seq = input_seq[1:]
+            else:
+                raise ValueError(f'Unexpected missmatch between bases in aligned_seq ({b1}) and input_seq ({input_seq[0]})')
+    
+    return new_data
