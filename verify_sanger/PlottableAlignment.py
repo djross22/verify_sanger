b72822978b8a82a7095a608ad0e68ba9ae08e15a
David Ross <david.ross@nist.gov>
2021-07-30 17:39:04 -0400

    Add additional attributes/properties to PlottableAlignment

--- verify_sanger/PlottableAlignment.py
+++ verify_sanger/PlottableAlignment.py
@@ -6,11 +6,28 @@ Created on Sun Jul 18 19:14:38 2021
 """
 
 from Bio.Align import PairwiseAlignment
+from verify_sanger.PlottableRecord import PlottableRecord 
 
 class PlottableAlignment(PairwiseAlignment):
-    def __init__(self, input_alignment):
+    def __init__(self, input_alignment,
+                 consensus_seq,
+                 mismatch_ind,
+                 record1,
+                 record2):
+        
         super().__init__(target=input_alignment.target, 
                          query=input_alignment.query, 
                          path=input_alignment.path, 
                          score=input_alignment.score,
                          )
+        self.consensus_seq = consensus_seq
+        self.mismatch_ind = mismatch_ind
+        
+        align_str = self.align_str
+        self.record1 = PlottableRecord.make_aligned_record(record1, align_str[0])
+        self.record2 = PlottableRecord.make_aligned_record(record2, align_str[2])
+    
+    @property
+    def align_str(self):
+        return f'{self}'.split('\n')
+        
