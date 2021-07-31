41f947ab0e931190f1bdc615bbe61b9e2040facc
David Ross <david.ross@nist.gov>
2021-07-30 21:03:08 -0400

    Add map_coordinate() method to help with shifting squence features

--- verify_sanger/PlottableAlignment.py
+++ verify_sanger/PlottableAlignment.py
@@ -30,4 +30,48 @@ class PlottableAlignment(PairwiseAlignment):
     @property
     def align_str(self):
         return f'{self}'.split('\n')
+    
+    
+    def map_coordinate(self, input_index, target=True):
+        """
+        maps a location in one of the input sequences used for the alignment
+        to a locaiton in the corresponding output sequence
+
+        Parameters
+        ----------
+        input_index : int
+            The position within the input sequence to be mapped
+        target : Boolean, optional
+            If True, the input sequence is self.target; 
+            if False, the input sequence is self.query. 
+            The default is True.
+
+        Returns
+        -------
+        output_index : int
+            The index in the output sequence that aligns with 
+            input_indes in the input sequence.
+
+        """
+        input_seq = self.target if target else self.query
+        output_seq = self.record1.seq if target else self.record2.seq
+        
+        # Make sure the input_index is withing the input sequence
+        if input_index < 0:
+            raise ValueError("The input_index ({input_index}) must be non-negative")
+        elif input_index >= len(input_seq):
+            raise ValueError(f"The input_index ({input_index}) must less than the length of the input sequence ({len(input_seq)})")
+        
+        i = 0
+        for j, b in enumerate(output_seq):
+            if b == input_seq[i]:
+                if i == input_index:
+                    break
+                else:
+                    i += 1
+            elif b != '-':
+                # Each base, b, should match or be a dash; if not, something weird has happended 
+                print(f'Unexpected base missmatch in map_coordinate(input_index={input_index}, target={target})')
+        
+        return j
         
